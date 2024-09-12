be2f4523b9ee76f5599a2dccd7ac8b05be31cde7
mini2929 <haloyun29@gmail.com>
2024-09-12 16:56:16 +0900

    일단 선생님꺼 따라침

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const btnReset = document.querySelectorAll("button")[0];
 const btnActive = document.querySelectorAll("button")[1];
 const box = document.querySelector("#box");
+
+/*
+DomEl.classList.add('클래스명') : 요소에 해당 클래스명 추가
+DomEl.classList.remove('클래스명') : 요소에 해당 클래스명 제거
+DomEl.classlist.remove('클래스명') : 요소에 있는 모든 클래스명 제거
+DomEl.classlist.toggle('클래스명') : 해당 클래스명이 있으면 제거하고 없으면 추가
+DomEl.classlist.contains('클래스명') 해당 클래스명이 있으면 true 반환 없으면 false 반환
+*/
+
+btnActive.addEventListener("click", () => {
+    box.classList.add("on");
+});
+
+btnReset.addEventListener("clikc", () => {
+    box.classList.remove("on");
+});
